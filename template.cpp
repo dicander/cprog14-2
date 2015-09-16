8c3c4d1482192acaba526662042772e6ba58557f
Marcus Dicander <dicander@gmail.com>
2015-09-16 15:18:30 +0200

    added contest template

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, from, to) for(int i=from;i<int(to);++i)
+#define trav(a,x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+  cin.sync_with_stdio(false);
+  return 0;
+}
