af8f19e5e7a3af1ac0d66db93a3402bd4d59f99e
Marcus Dicander <dicander@kth.se>
2019-01-28 00:20:02 +0100

    Added structure and a todo

--- classes/simpleclass.cpp
+++ classes/simpleclass.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
-struct foo{
+
+
+struct foo {
   foo(string s):name(s){};
   string name;
 };
 
-int main(){
+
+int main() {
   string m("a");
   foo f(m);
   return 0;
